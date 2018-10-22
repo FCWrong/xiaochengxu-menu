ff7d71d27a136ab1a0c1d3b44a19d762ce7a4ee1
冯臣 <chenchen.feng@gaeamobile.com>
2018-10-22 18:07:53 +0800

    长按删除

--- demo_yun/miniprogram/pages/todo/todo.js
+++ demo_yun/miniprogram/pages/todo/todo.js
@@ -12,8 +12,8 @@ Page({
     //   { name: 'standard is dealicient for u.standard is dealicient for u.standard is dealicient for u.standard is dealicient for u.standard is dealicient for u.', value: '1' }
     // ],
 
-    items: [{ name: 'standard is dealt for u.', value: '0' },
-      { name: 'standard is dealicient for u.standard is dealicient for u.standard is dealicient for u.standard is dealicient for u.standard is dealicient for u.', value: '1' }],
+    items: [{ name: '集结部队', value: '0', checked: true },
+    { name: 'standard is dealicient for u.standard is dealicient for u.standard is dealicient for u.standard is dealicient for u.standard is dealicient for u.', value: '1' }],
 
     startX: 0, //开始坐标
 
@@ -22,7 +22,9 @@ Page({
     tabs: ["待办", "完成"],
     activeIndex: 0,
     sliderOffset: 0,
-    sliderLeft: 0
+    sliderLeft: 0,
+
+    disabled_checkbox: false
   },
 
   tabClick: function (e) {
@@ -49,6 +51,29 @@ Page({
 
     this.setData({
       items: items
+    });
+  },
+
+  onDel: function (e) {
+    console.log('onDel，携带value值为：', e);
+    this.setData({
+      disabled_checkbox: false
+    })
+    var that = this;
+    wx.showActionSheet({
+      itemList: ['删除'],
+      itemColor: "#e64340",
+      success: function (res) {
+        if (!res.cancel) {
+          console.log(res.tapIndex)
+        }
+      },
+      fail: function (res) {
+     
+      },
+      complete: function (res) {
+       
+      }
     });
   },
 
@@ -115,124 +140,4 @@ Page({
   onShareAppMessage: function () {
 
   },
-
-  //手指触摸动作开始 记录起点X坐标
-
-  touchstart: function (e) {
-
-    //开始触摸时 重置所有删除
-
-    this.data.items.forEach(function (v, i) {
-
-      if (v.isTouchMove) //只操作为true的
-
-        v.isTouchMove = false;
-
-    })
-
-    this.setData({
-
-      startX: e.changedTouches[0].clientX,
-
-      startY: e.changedTouches[0].clientY,
-
-      items: this.data.items
-
-    })
-
-  },
-
-  //滑动事件处理
-
-  touchmove: function (e) {
-
-    var that = this,
-
-      index = e.currentTarget.dataset.index, //当前索引
-
-      startX = that.data.startX, //开始X坐标
-
-      startY = that.data.startY, //开始Y坐标
-
-      touchMoveX = e.changedTouches[0].clientX, //滑动变化坐标
-
-      touchMoveY = e.changedTouches[0].clientY, //滑动变化坐标
-
-      //获取滑动角度
-
-      angle = that.angle({
-        X: startX,
-        Y: startY
-      }, {
-          X: touchMoveX,
-          Y: touchMoveY
-        });
-
-    that.data.items.forEach(function (v, i) {
-
-      v.isTouchMove = false
-
-      //滑动超过30度角 return
-
-      if (Math.abs(angle) > 30) return;
-
-      if (i == index) {
-
-        if (touchMoveX > startX) //右滑
-
-          v.isTouchMove = false
-
-        else //左滑
-
-          v.isTouchMove = true
-
-      }
-
-    })
-
-    //更新数据
-
-    that.setData({
-
-      items: that.data.items
-
-    })
-
-  },
-
-  /**
-  
-  * 计算滑动角度
-  
-  * @param {Object} start 起点坐标
-  
-  * @param {Object} end 终点坐标
-  
-  */
-
-  angle: function (start, end) {
-
-    var _X = end.X - start.X,
-
-      _Y = end.Y - start.Y
-
-    //返回角度 /Math.atan()返回数字的反正切值
-
-    return 360 * Math.atan(_Y / _X) / (2 * Math.PI);
-
-  },
-
-  //删除事件
-
-  del: function (e) {
-
-    this.data.items.splice(e.currentTarget.dataset.index, 1)
-
-    this.setData({
-
-      items: this.data.items
-
-    })
-
-  }
 })
