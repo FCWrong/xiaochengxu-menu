ee5ff0020f90f90b66922ea8c5719ff020c5d45e
冯臣 <chenchen.feng@gaeamobile.com>
2018-10-26 18:45:11 +0800

    基本操作完成

--- demo_yun/miniprogram/pages/qingdan/qingdan.js
+++ demo_yun/miniprogram/pages/qingdan/qingdan.js
@@ -5,48 +5,40 @@ Page({
    * 页面的初始数据
    */
   data: {
-    radioItems: [{
-        name: 'cell standard',
-        value: '0'
-      },
-      {
-        name: 'cell standard',
-        value: '1',
-        checked: true
-      }
-    ],
-    checkboxItems: [{
-        name: 'standard is dealt for u.',
-        value: '0',
-        checked: true
-      },
-      {
-        name: 'standard is dealicient for u.',
-        value: '1'
-      },
-      {
-        name: 'standard is dealicient for u.standard is dealicient for u.standard is dealicient for u.standard is dealicient for u.',
-        value: '1'
-      },
-      {
-        name: 'A5空白素描本欧美圣经本超厚笔记本文具加厚大号A4日记本记事本子白纸速写本手绘画画简约复古手账本手帐本',
-        value: '1'
-      },
-    ],
 
     isShowDemo: false,
+    isTop: false,
     isY: true,
+    upper: -50,
 
     todolist: null,
 
     pageIndex: 0
 
   },
-  //改变状态
-  checkboxChange: function(e) {
+  //切换分组
+  swiperChange: function (e) {
+    console.log("swiperChange:", e);
+    this.setData({
+      pageIndex: e.detail.current
+    })
+  },
 
+  //时间戳转日期
+  timeToData: function (e) {
+    var date = new Date(e);
+    return (date.getMonth() + 1) + "-" + (date.getDate())
+  },
+  //是否是同一天
+  isSameDay: function (now, value) {
+    return this.timeToData(now) == value
+  },
+
+  //改变状态
+  checkboxChange: function (e) {
+    console.log("checkbox:",e.detail.value)
     var arr = e.currentTarget.dataset.grouptgroup.split(";");
-    
+
     var values = e.detail.value;
     var todolist = this.data.todolist;
     var items = todolist[arr[0]][arr[1]].todoList;
@@ -54,7 +46,7 @@ Page({
     for (var i = 0, lenI = items.length; i < lenI; ++i) {
       items[i].isDone = false;
 
-      for (var j = 0, lenJ = values.length; j < lenJ; ++j) {  
+      for (var j = 0, lenJ = values.length; j < lenJ; ++j) {
         if (items[i].id == values[j]) {
           items[i].isDone = true;
           break;
@@ -71,7 +63,7 @@ Page({
     } catch (e) { }
   },
   //获取分组方法
-  getToDoList: function() {
+  getToDoList: function () {
     try {
       var key = "ToDoList"
       var value = wx.getStorageSync(key)
@@ -80,86 +72,206 @@ Page({
         this.setData({
           todolist: value
         })
+      } else {
+        var now = Date.now()
+        var time = this.timeToData(Date.now())
+        var todolist = [
+          [
+            {
+              title: "10-25",
+              time: now,
+              todoList: [
+                {
+                  "id": now + 1,
+                  "time": now + 1,
+                  "content": "下划创建 清单",
+                  "isDone": false
+                },
+                {
+                  "id": now,
+                  "time": now,
+                  "content": "长按删除 清单",
+                  "isDone": false
+                },
+                {
+                  "id": now - 1,
+                  "time": now - 1,
+                  "content": "右划创建 分组",
+                  "isDone": false
+                }
+              ]
+            },
+            {
+              title: "10-24",
+              time: now - 1,
+              todoList: [
+                {
+                  "id": now + 1,
+                  "time": now + 1,
+                  "content": "下划创建 清单",
+                  "isDone": false
+                },
+                {
+                  "id": now,
+                  "time": now,
+                  "content": "长按删除 清单",
+                  "isDone": false
+                },
+                {
+                  "id": now - 1,
+                  "time": now - 1,
+                  "content": "右划创建 分组",
+                  "isDone": false
+                }
+              ]
+            }
+          ]
+        ]
+
+        this.setData({
+          todolist: todolist
+        })
+
+        try {
+          wx.setStorageSync('ToDoList', this.data.todolist)
+        } catch (e) { }
       }
     } catch (e) {
       console.log("Error-getGroupList:", e)
     }
   },
-  setinfo: function() {
-    var info = [
-      [{
-        "title": 123,
-        "todoList": [{
-          "id": 1,
-          "time": 1540475683,
-          "content": "这是啊是大是",
-          "isDone": false
-        }]
-      },
-      {
-        "title": 1540232475683,
-        "todoList": [{
-          "id": 2,
-          "time": 1540475683,
-          "content": "121212",
-          "isDone": false
-        }]
-      },
-      {
-        "title": 1540475683,
-        "todoList": [{
-          "id": 3,
-          "time": 1540475683,
-          "content": "啊飒333333飒阿三",
-          "isDone": false
-        }]
-      }
-      ],
-      [{
-        "title": 1540475683,
-        "todoList": [{
-          "id": 4,
-          "time": 1540475683,
-          "content": "啊飒飒343434阿三",
-          "isDone": false
-        }]
-      },
-      {
-        "title": 1540475683,
-        "todoList": [{
-          "id": 5,
-          "time": 1540475683,
-          "content": "啊飒343443飒阿三",
-          "isDone": false
-        }]
-      }
-      ]
-    ];
+
+  onLoad: function (options) {
 
     try {
-      wx.setStorageSync('ToDoList', info)
+      wx.clearStorageSync()
     } catch (e) {
-      console.log("error", e);
+      // Do something when catch error
     }
-  },
-
-  onLoad: function(options) {
-    // this.setinfo();
     this.getToDoList();
   },
 
-  onTop: function(e) {
+  onTop: function (e) {
     console.log("OnTop:", e)
     this.setData({
-      isShowDemo: true,
-      isY: false
+      isTop: true,
     });
   },
-  inputBlur: function(e) {
-    console.log("Blur:", e)
+  touchStart: function (e) {
+    console.log("toucheStart", e);
+
+    this.setData({
+      isTop: false
+    });
+  },
+  touchEnd: function (e) {
+    console.log("toucheEnd", e);
+    if (this.data.isTop) {
+
+      var time = this.timeToData(Date.now());
+      var todolist = this.data.todolist;
+      var groupList = todolist[this.data.pageIndex];
+
+      if (!this.isSameDay(Date.now(), groupList[0].title)) {
+        var group = {
+          title: time,
+          time: Date.now(),
+          todoList: [
+            {
+              id: Date.now(),
+              time: Date.now(),
+              content: "",
+              isDone: false
+            }
+          ]
+        }
+        groupList.unshift(group);
+
+        this.setData({
+          todolist: todolist,
+        });
+      } else {
+        var todo = {
+          id: Date.now(),
+          time: Date.now(),
+          content: "",
+          isDone: false
+        }
+
+        groupList[0].todoList.unshift(todo);
+
+        this.setData({
+          todolist: todolist,
+        });
+      }
+
+      this.setData({
+        isShowDemo: true,
+        isY: false,
+        upper: -5000
+      })
+
+    }
+  },
+  //提交todo
+  confirm: function (e) {
+    console.log("confirm:", e);
+    var time = Date.now();
+    var value = e.detail.value;
+
+    if(value==""){
+      this.inputBlur(e)
+      return
+    }
+
+    var arr = e.currentTarget.dataset.grouptgroup.split(";");
+    var todolist = this.data.todolist;
+    var firstTitle = todolist[arr[0]][arr[1]].title;
+
+    if (this.isSameDay(time, firstTitle)) {
+
+      todolist[arr[0]][arr[1]].todoList[arr[2]].content = value
+
+      this.setData({
+        todolist: todolist,
+      })
+
+      try {
+        wx.setStorageSync('ToDoList', this.data.todolist)
+      } catch (e) { }
+    }
+
     this.setData({
       isShowDemo: false,
-      isY: true
+      isY: true,
+      upper: -50
+    })
+
+  },
+  inputBlur: function (e) {
+    console.log("Blur:", e)
+
+    var time = this.timeToData(Date.now());
+    var todolist = this.data.todolist;
+    var groupList = todolist[this.data.pageIndex];
+
+    var item = groupList[0];
+    if (item != null) {
+      if (item.todoList.length == 1){
+        groupList.splice(0,1)  
+      }else{
+        item.todoList.splice(0,1)
+      } 
+
+      this.setData({
+        todolist: todolist,
+      })    
+    }
+
+    this.setData({
+      isShowDemo: false,
+      isY: true,
+      upper: -50
     });
   },
 
@@ -171,49 +283,49 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function() {
+  onReady: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
+  onShow: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {
+  onHide: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {
+  onUnload: function () {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
+  onPullDownRefresh: function () {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
+  onReachBottom: function () {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {
+  onShareAppMessage: function () {
 
   }
 })
